e0341d8f56cb7d54bb03b037043dcb1d427ba0f9
okokoh1995 <mondaycokokoh@gmail.con>
2022-08-12 11:26:56 +0100

    new form to my article

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Form from "./component/Form";
+import "./style.css";
 
-function App() {
+// import Header from "./Header"
+// import Body from "./Body"
+
+function App(props) {
+  // const [messages, setMessages] = React.useState(["a", "b"])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Form /> 
+      {/* {
+          messages.length === 0 ?
+          <h1>You're all caught up!</h1> :
+          <h1>You have {messages.length} unread {messages.length > 1 ?  "messages" : "message"} </h1>
+      // } */}
     </div>
-  );
+    // <main>
+    //   <Header user={props.user} />
+    //   <Body use={props.user} />
+    // </main>
+  )
 }
 
-export default App;
+ export default App;
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header(props) {
+    
+
+    return (
+        <Header>
+            <p>Current use: {props.user}</p>
+        </Header>
+    )
+}
